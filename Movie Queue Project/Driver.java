c75fab26fac4a20742f1bcf622f850ac2e5722db
B-CWebb <webb.brandon.c.1@gmail.com>
2025-01-05 19:30:49 -0600

    Add files via upload

---
+++ Movie Queue Project/Driver.java
@@ -0,0 +1,23 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		MovieQueue ms = new MovieQueue();
+		Movie A = new Movie("Texas Chainsaw Massacre", "Horror");
+		Movie B = new Movie("Choose or Die", "Horror");
+		Movie C = new Movie("No One Gets Out Alive", "Horror");
+		ms.add(A);
+		ms.add(B);
+		ms.add(C);
+		
+		
+		if (C.equals(ms.remove())) {System.out.println("1");}
+		else {System.out.println("0"); }
+		if (B.equals(ms.remove())) {System.out.println("1");}
+		else {System.out.println("0"); }
+		if (A.equals(ms.remove())) {System.out.println("1");}
+		else {System.out.println("0"); }
+	}
+
+	
+}
---
+++ Movie Queue Project/MovieQueue.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+
+
+public class MovieQueue {
+	private ArrayList<Movie> movQ = new ArrayList<Movie>(10);
+	int lastHorror;
+	
+	public void add(Movie movie) {
+					if ((movie.getGenre()=="Horror") && (movQ.equals(null))){
+				movQ.add(movie);
+				lastHorror = 0;
+			}
+			else if ((movie.getGenre() == "Horror") && (lastHorror >= 0)) {
+				lastHorror += 1;
+				movQ.add(lastHorror, movie); }
+			
+			else {movQ.add(movie);
+			}
+		
+	}
+	
+	public Movie remove() {
+		if (movQ.equals(null)) {return null;}
+		else {return movQ.remove(0);}
+		
+	}
+}
